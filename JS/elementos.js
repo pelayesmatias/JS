8e67dfbcedb62bfd15d5c021eebc035a0d2fcc32
Matias Pelayes <pelayesleonardomatias@gmail.com>
2021-09-03 21:26:13 -0300

    Desafío: Crear un algoritmo utilizando un ciclo

--- JS/elementos.js
+++ JS/elementos.js
@@ -1,20 +1,77 @@
-let diasAlquiler = prompt("Indique cantidad de dias de alquiler");
+let metodo = prompt("¿Desea alquilar o comprar");
+const agradecimiento = "Gracias por contactarse con nosotros"
 
-if(diasAlquiler<=3){
-    alert("Pagás alquiler por día");
-}
-else if ((diasAlquiler>=4) && (diasAlquiler<=7)){
-    alert("Super promo: Pagás alquiler 4 días!");
+while (metodo != "") {
+    switch (metodo.toUpperCase()) {
+        case "COMPRAR":
+            const provincia = prompt("¿De que provincia sos?");
+            
+            if (provincia.toUpperCase () == "MENDOZA") {
+                alert("Genial! El envío es gratis!");
+                alert(agradecimiento)
+                metodo = ""
+            }
+            
+            else {
+               const otraProvincia = prompt ("¿Te gustaría que te presupuestemos el envío?")
+                if (otraProvincia.toUpperCase() == "SI") {
+                    prompt("ingresá tu correo electrónico y te contactaremos");
+                    alert(agradecimiento)
+                    metodo = ""
+                }
+
+                else {
+
+                    alert(agradecimiento);
+                    metodo = ""
+                }
+            }
+            break
+
+        case "ALQUILAR":
+            alert("El servicio de alquiler se encuentra momentáneamente suspendido")
+            alert(agradecimiento);
+            metodo = ""
+
+            break
+        default:
+            alert("No podemos procesar su solicitud")
+            metodo = prompt("¿Desea alquilar o comprar");
+            break
+    }
+
 
 }
-else if ((diasAlquiler>=8) && (diasAlquiler<=15)){
-    alert("Nos volvimos locos: Pagas solo 6 días");
 
-}
-else if ((diasAlquiler>=16) && (diasAlquiler<=30)){
-    alert("Nos volvimos locos: Pagas solo 10 días");
 
-}
-else {
-    alert("Comunicate con nosotros y te presupuestamos la mejor opción");
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
